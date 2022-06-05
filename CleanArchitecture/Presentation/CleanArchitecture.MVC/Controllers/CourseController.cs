ffd2e3e7086830790feeb6f8926a4633c89117e3
Lưu Trọng Tấn <tanluutrong2206@gmail.com>
2022-06-05 17:44:37 +0700

    update

---
+++ CleanArchitecture/Presentation/CleanArchitecture.MVC/Controllers/CourseController.cs
@@ -0,0 +1,30 @@
+﻿using CleanArchitecture.Application.Interfaces;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClearnArchitecture.MVC.Controllers
+{
+    [Authorize]
+    public class CourseController : Controller
+    {
+        private readonly ICourseService _service;
+
+        public CourseController(ICourseService service)
+        {
+            _service = service;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var model = await _service.GetCoursesAsync();
+            return View(model);
+        }
+    }
+}
